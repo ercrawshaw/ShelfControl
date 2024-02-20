5cb5828208b8e900a438cedc3eb9061e8b9d6481
Your Nameellecrawshaw <eleanorcrawshaw96@gmail.com>
2024-02-20 16:45:09 +0000

    style page

--- components/ManualSearch.js
+++ components/ManualSearch.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { Keyboard, StyleSheet, Text, TextInput, View, Button, Image, ScrollView } from 'react-native';
+import { Keyboard, StyleSheet, View, Image, ScrollView } from 'react-native';
 import React from 'react';
 import { useState } from 'react';
 import axios from 'axios';
-import { Card } from '@rneui/base';
 import { useForm } from 'react-hook-form';
+import { Searchbar } from 'react-native-paper';
+import { Button, Card, TextInput, Text } from 'react-native-paper';
 
 
 
 
-export default function ManualSearch() {
+
+export default function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [bookTitle, setBookTitle] = useState('');
   const [books, setBooks] = useState([]);
@@ -22,6 +24,9 @@ export default function ManualSearch() {
   const [manualAuthorName, setManualAuthorName] = useState('');
   const [manualPublishDate, setManualPublishDate] = useState('');
   const [manuallyAddedBook, setMannuallyAddedBook] = useState({});
+  const [bookAdded, setBookAdded] = useState(false);
+  const [infoNeeded, setInfoNeeded] = useState('');
+
 
   function searchBook() {
     Keyboard.dismiss();
@@ -48,155 +53,230 @@ export default function ManualSearch() {
         }
       })
     }
-    };
+  };
 
-    function handleAddClick () {
-      setManualAdd(true)
-    };
+  function handleAddClick () {
+    setManualAdd(true)
+  };
 
-    function handleManualBookAdd () {
-      console.log(manualAuthorName, manualBookTitle, manualPublishDate)
+
+  function handleManualBookAdd () {
+
+    if (manualAuthorName === '' || manualBookTitle === '') {
+        setInfoNeeded('Need to fill out title and author name!')
+    }else{
+      setInfoNeeded('')
       setMannuallyAddedBook({
         title: manualBookTitle,
-        author: ma
+        author: manualAuthorName,
+        publication: manualPublishDate
       })
-    };
+      setBookAdded(true)
+    }   
+  };
+
+  function handleCancelClick () { 
+    setBookAdded(false) 
+    setManualBookTitle('')
+    setManualAuthorName('')
+    setManualPublishDate('')
+  };
+
+  function handleEditClick () {
+    setBookAdded(false) 
+  };
+
       
-
-
-
+  //Search Options Chosen
   if (books.length === 0 && !manualAdd) {
     return (
-      <View style={styles.container}>
-        <Button 
-        title="Manual Add"
-        onPress={handleAddClick}
-         />
-        <TextInput 
-          placeholder="Search title..."
-          value={bookTitle}
-          style={{
-            backgroundColor: 'lightblue'
-          }}
-          onChangeText={(bookTitle) => setBookTitle(bookTitle)}
-        />
-        <Button 
-        title="Search"
-        onPress={searchBook}
-        color='lightgreen'
-        accessibilityLabel='Search for a book title'
-        />
-        <Text>{alert}</Text>
-        <Text>{error}</Text>
-        <StatusBar style="auto" />
-      </View>
+    <View style={styles.searchbar}>
+
+    <Button icon="plus" mode="contained" onPress={handleAddClick}>
+    Add a Book
+    </Button>
+
+    <Searchbar
+    placeholder="Search Book Title"
+    onChangeText={(bookTitle) => setBookTitle(bookTitle)}
+    value={bookTitle}
+    onIconPress={searchBook}
+    />  
+    <Text>{alert}</Text>
+    <Text>{error}</Text>
+    <StatusBar style="auto" />
+    </View>
     );
+
+  // Books shown from Google Books Api
   }else if (books.length !== 0 && !manualAdd){
     return (
-      <View style={styles.container}>
-        <Button 
-        title="Manual Add"
+    <View style={styles.searchbar}>
 
-        />
-        <TextInput 
-          placeholder="Search title..."
-          value={bookTitle}
-          style={{
-            backgroundColor: 'lightblue'
-          }}
-          onChangeText={(bookTitle) => setBookTitle(bookTitle)}
-        />
-         <Button 
-        title="Search"
-        onPress={searchBook}
-        color='lightgreen'
-        accessibilityLabel='Search for a book title'
-        />
-        <StatusBar style="auto" />
-        <ScrollView>
+    <Button icon="plus" mode="contained" onPress={handleAddClick}>
+    Add a Book
+    </Button>
+
+    <Searchbar
+      placeholder="Search Book Title"
+      onChangeText={(bookTitle) => setBookTitle(bookTitle)}
+      value={bookTitle}
+      onIconPress={searchBook}
+    />  
+
+    <StatusBar style="auto" />
+      <ScrollView>
         <View style={styles.container}>
         {books.map((book, i) => {
-          //const image = book.volumeInfo.imageLinks.thumbnail;
-          return (
-            
-            <Card key={i} style={styles.container}>
-            <View>
-              <Text>title: {book.volumeInfo.title}</Text>
-              <Text>author: {book.volumeInfo.authors}</Text>
-              <Button title="Add Book">Add Book!</Button>
-            </View>
-            </Card>
-            
+          //issue with thumbnail at times
+        return (
+        
+        <Card key={i} style={styles.bookcard}>
+        <Card.Title title={book.volumeInfo.title} subtitle={book.volumeInfo.authors}  />
+        <Card.Cover style={styles.bookcover} source={{ uri: book.volumeInfo.imageLinks.thumbnail }} />
+          <Card.Actions>
+            <Button>Add Book</Button>
+          </Card.Actions>
+        </Card>
+
           )
         })}
         </View>
         </ScrollView>
       </View>
     );
+
+  //Manually adding book information
   }else if (manualAdd) {
-    return (
-      <View style={styles.container}>
-        <Button 
-        title="Manual Add"
 
-        />
-        <TextInput 
-          placeholder="Search title..."
-          value={bookTitle}
-          style={{
-            backgroundColor: 'lightblue'
-          }}
-          onChangeText={(bookTitle) => setBookTitle(bookTitle)}
-        />
-         <Button 
-        title="Search"
-        onPress={searchBook}
-        color='lightgreen'
-        accessibilityLabel='Search for a book title'
-        />
+    //Manually adding form
+    if (!bookAdded) {
+      return (
+      <View style={styles.searchbar}>
 
+      <Button icon="plus" mode="contained" onPress={handleAddClick}>
+      Add a Book
+      </Button>
 
-      <View style={styles.container}>
-      <Text>Manual Add</Text>
+      <Searchbar
+      placeholder="Search Book Title"
+      onChangeText={(bookTitle) => setBookTitle(bookTitle)}
+      value={bookTitle}
+      onIconPress={searchBook}
+      />  
+
+      <View style={styles.bookForm}>
+      <Text style={styles.bookFormText} variant="headlineLarge">Add your book details</Text>
       <TextInput
-        style={styles.input}
-        autoCompleteType="username"
-        placeholder="Book title"
-        value={manualBookTitle}
-        onChangeText={(manualBookTitle) => setManualBookTitle(manualBookTitle)}
+      style={styles.bookFormComponent}
+      label="Book Title"
+      value={manualBookTitle}
+      onChangeText={(manualBookTitle) => setManualBookTitle(manualBookTitle)}
       />
       <TextInput
-        style={styles.input}
-        autoCompleteType="email"
-        keyboardType="email-address"
-        textContentType="emailAddress"
-        placeholder="Author name"
-        value={manualAuthorName}
-        onChangeText={(manualAuthorName) => setManualAuthorName(manualAuthorName)}
+      style={styles.bookFormComponent}
+      label="Book Author"
+      value={manualAuthorName}
+      onChangeText={(manualAuthorName) => setManualAuthorName(manualAuthorName)}
       />
       <TextInput
-        style={styles.input}
-        autoCompleteType="password"
-        placeholder="Publication Year"
-        value={manualPublishDate}
-        onChangeText={(manualPublishDate) => setManualPublishDate(manualPublishDate)}
+      style={styles.bookFormComponent}
+      label="Year Published"
+      value={manualPublishDate}
+      keyboardType='numeric'
+      onChangeText={(manualPublishDate) => setManualPublishDate(manualPublishDate)}
       />
+
+    <Button 
+    style={styles.bookFormComponent}
+    icon="camera" 
+    mode="contained" 
+    >
+    Add a cover
+    </Button>
+
+    <Button 
+    style={styles.bookFormComponent}
+    icon={{ uri: 'https://www.iconpacks.net/icons/2/free-opened-book-icon-3163-thumb.png' }} 
+    mode="contained" 
+    onPress={handleManualBookAdd}
+    title="Check Book Details">
+    Check Details
+    </Button>   
+
+      <Text style={styles.bookFormText} >{infoNeeded}</Text>
       <Button 
-      title="Add Book"
+      title="Check Book Details"
       onPress={handleManualBookAdd}
        />
-      
-
     </View>
+    </View>
+    )
+
+
+  //Book information displayed for check before adding  
+  }else{
+    return (
+      <View style={styles.searchbar}>
+
+      <Button icon="plus" mode="contained" onPress={handleAddClick}>
+      Add a Book
+      </Button>
+
+      <Searchbar
+      placeholder="Search Book Title"
+      onChangeText={(bookTitle) => setBookTitle(bookTitle)}
+      value={bookTitle}
+      onIconPress={searchBook}
+      /> 
+
+      <Card>
+        <Card.Content>
+          <Text variant="titleLarge">Title: {manuallyAddedBook.title}</Text>
+          <Text variant="bodyMedium">Author: {manuallyAddedBook.author}</Text>
+          <Text variant="bodyMedium">Published: {manuallyAddedBook.publication}</Text>
+        </Card.Content>
+      <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+        <Card.Actions>
+          <Button 
+          icon={{ uri: 'https://icons.veryicon.com/png/o/miscellaneous/medium-thin-linear-icon/cross-23.png'}} 
+          onPress={handleCancelClick} 
+          />
+          <Button onPress={handleEditClick} >Edit Book</Button>
+          <Button>Add Book</Button>
+        </Card.Actions>
+      </Card>
+
       </View>
     )
   }
-
-
-  
+ }  
 }
 
 const styles = StyleSheet.create({
+  bookFormText: {
+    textAlign: 'center',
+  },
+  bookFormComponent: {
+    margin: 10,
+  },
+  bookForm: {
+    margin: 40,
+  },
+  bookcover: {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '55%',
+  },
+  bookcard: {
+    margin: 10,
+    marginBottom: 50,
+    width: '70%',
+  },
+  searchbar: {
+    marginTop: 30,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
