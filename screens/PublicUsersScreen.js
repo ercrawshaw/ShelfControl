4fea171e738c4e7ea155d94d0fb99435ee2f1eb3
AudeNgD <aude.nguyenduc@gmail.com>
2024-02-22 15:35:42 +0000

    update user creation with boolean private key

---
+++ screens/PublicUsersScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const PublicUsersScreen = () => {
+  return (
+    <View>
+      <Text>PublicUsersScreen</Text>
+    </View>
+  );
+};
+
+export default PublicUsersScreen;
+
+const styles = StyleSheet.create({});
